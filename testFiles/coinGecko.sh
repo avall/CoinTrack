e1abfac783254e5c19636ea4f6489edee5bff033
tituz <linux@tituz.com>
2024-04-02 09:48:30 +0200

    transfered to json

--- testFiles/coinGecko.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#curl -X -s 'GET' 'https://api.coingecko.com/api/v3/coins/list?include_platform=false' -H 'accept: application/json' | jq > cg.json
-
-
-n=$(jq .[].name cg.json | wc -l)
-
-
--- testFiles/coincap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl --location 'api.coincap.io/v2/assets/bitcoin/history?interval=d1'
-
-curl --location 'api.coincap.io/v2/assets/alchemy-pay/history?interval=d1' | jq
